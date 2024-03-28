92258a5850a62ff0897f081a9b519cd1f236b6d8
Furkan Doğu <furkandogu2018@gmail.com>
2024-03-28 21:28:35 +0300

    bitti. cors yapılacak

--- app/controllers/book.controller.js
+++ app/controllers/book.controller.js
@@ -23,6 +23,43 @@ module.exports = {
             result: data.dataValues
         })
     
+    },
+
+    read: async (req, res) => {
+
+        const data = await Book.findByPk(req.params.id)
+
+        res.status(200).send({
+            error: false,
+            result: data
+        })
+
+    },
+
+    update: async (req, res) => {
+
+        const data = await Book.update(req.body, {where: {id: req.params.id}})
+
+        res.status(202).send({
+            error: false,
+            message: "Updated",
+            result: data,
+            new: await Book.findByPk(req.params.id)
+        })
+
+    },
+
+    delete: async (req, res) => {
+
+        const data = await Book.destroy({where: {id: req.params.id}})
+
+        if(data > 0) {
+            res.sendStatus(204)
+        } else {
+            res.errorStatusCode = 404
+            throw new Error("Not Found")
+        }
+
     }
 
 }
--- app/routes/book.router.js
+++ app/routes/book.router.js
@@ -8,4 +8,10 @@ router.route("/")
     .get(book.list)
     .post(book.create)
 
+router.route("/:id")
+    .get(book.read)
+    .put(book.update)
+    .patch(book.update)
+    .delete(book.delete)
+
 module.exports = router
