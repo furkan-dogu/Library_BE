33885475692d8bd6021a74ab9fde9f67ea8e723a
Furkan Doğu <furkandogu2018@gmail.com>
2024-03-31 13:27:38 +0300

    güncelleme

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8000
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://library-fe-two.vercel.app/"
 }))
 
 require("express-async-errors")
